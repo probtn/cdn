bc2c481a895b9f1d7c88210f0e0ad48e1b2b745b
Anton Lenev <m0rg0t.Anton@gmail.com>
2015-10-23 23:36:01 +0300

    bower

--- precommit.js
+++ precommit.js
@@ -4,9 +4,11 @@ var exec = require('child_process').exec;
 // https://npmjs.org/package/execSync
 // Executes shell commands synchronously
 var sh = require('sync-exec').run;
+
+console.log("test");
  
 exec('grunt', function (err, stdout, stderr) {
- 
+console.log(0);
 console.log(err);
 console.log(stdout);
 
@@ -16,13 +18,38 @@ console.log(stdout);
   //if (err) {
 	  
 		exec('cd bower', function (err, stdout, stderr) { 
+			console.log(1);
+			console.log(err);
+			console.log(stdout);
 			exec('git add .', function (err, stdout, stderr) { 
+				console.log(2);
+				console.log(err);
+				console.log(stdout);
+				console.log(stderr);
+			
 				exec('git commit -m "bower"', function (err, stdout, stderr) { 
+				console.log(3);
+				console.log(err);
+				console.log(stdout);
+			
 					exec('git push', function (err, stdout, stderr) { 
+						console.log(4);
+						console.log(err);
+						console.log(stdout);
+					
 						exec('cd ..', function (err, stdout, stderr) { 
+							console.log(5);
+							console.log(err);
+							console.log(stdout);
+							
 							exec('git add .', function (err, stdout, stderr) { 
+								console.log(6);
 								console.log(err);
 								console.log(stdout);
+								
+								var exitCode = 0;
+								process.exit(exitCode);
+								
 						    });
 						});
 					});
@@ -31,8 +58,12 @@ console.log(stdout);
 		});
 	  
 	  /*exec('git add .', function (err, stdout, stderr) { 
+		console.log(1);
 		console.log(err);
 		console.log(stdout);
+		
+		var exitCode = 0;
+		process.exit(exitCode);
 	  });*/
   //}
 	/*
