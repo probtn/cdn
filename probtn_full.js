d1f6636c102d0c71edaa70dcb1ed05860cb03aed
Anton Lenev <m0rg0t.Anton@gmail.com>
2016-02-15 20:50:44 +0300

    jsSHA

--- probtn_full.js
+++ probtn_full.js
@@ -14,6 +14,7 @@ function probtn_callPlayer(frame_id, func, args) {
 }
 
 /*jsSHA*/
+/*
 'use strict'; (function (H) {
     function v(c, a, b) {
         var g = 0, d = [], f = 0, e, h, n, l, m, F, r, p = !1, k = !1, q = [], t = [], u, y = !1; b = b || {}; e = b.encoding || "UTF8"; u = b.numRounds || 1; n = z(a, e); if (u !== parseInt(u, 10) || 1 > u) throw Error("numRounds must a integer >= 1"); F = function (a, b) { return A(a, b, c) }; r = function (a, b, f, d) {
@@ -65,7 +66,7 @@ function probtn_callPlayer(frame_id, func, args) {
     } var G;
     G = [1116352408, 1899447441, 3049323471, 3921009573, 961987163, 1508970993, 2453635748, 2870763221, 3624381080, 310598401, 607225278, 1426881987, 1925078388, 2162078206, 2614888103, 3248222580, 3835390401, 4022224774, 264347078, 604807628, 770255983, 1249150122, 1555081692, 1996064986, 2554220882, 2821834349, 2952996808, 3210313671, 3336571891, 3584528711, 113926993, 338241895, 666307205, 773529912, 1294757372, 1396182291, 1695183700, 1986661051, 2177026350, 2456956037, 2730485921, 2820302411, 3259730800, 3345764771, 3516065817, 3600352804, 4094571909,
     275423344, 430227734, 506948616, 659060556, 883997877, 958139571, 1322822218, 1537002063, 1747873779, 1955562222, 2024104815, 2227730452, 2361852424, 2428436474, 2756734187, 3204031479, 3329325298]; "function" === typeof define && define.amd ? define(function () { return v }) : "undefined" !== typeof exports ? "undefined" !== typeof module && module.exports ? module.exports = exports = v : exports = v : H.jsSHA = v
-})(this);
+})(this);*/
 
 /*
 function initTrackingLinkTest() {
