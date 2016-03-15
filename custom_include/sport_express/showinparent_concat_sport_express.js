63997e03110eb82cc9a546ca206b4d0a6c65fbfb
Anton Lenev <m0rg0t.Anton@gmail.com>
2016-03-15 14:19:13 +0300

    sport-express.ru custom include

---
+++ custom_include/sport_express/showinparent_concat_sport_express.js
@@ -0,0 +1,62 @@
+(function () {
+document.domain = "sport-express.ru";
+var oHead = window.top.document.getElementsByTagName('HEAD').item(0);
+
+function loadJS(src, callback) {
+    var s = window.top.document.createElement('script');
+    s.src = src;
+    s.async = true;
+    s.onreadystatechange = s.onload = function () {
+        var state = s.readyState;
+        if (!callback.done && (!state || /loaded|complete/.test(state))) {
+            callback.done = true;
+            callback();
+        }
+    };
+    window.top.document.getElementsByTagName('head')[0].appendChild(s);
+}
+
+function getParameterByName(name) {
+	try {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	} catch(ex) {
+	}
+}
+
+var params = {};
+params.dfp = {};
+params.dfp.isDFP = true;
+params.dfp.clickUrlEsc = getParameterByName("click_url_esc");
+params.dfp.cacheBuster = getParameterByName("cacheBuster");
+var domain = getParameterByName("domain");
+if ((domain!==null) && (domain!==undefined) && (domain!=="")) {
+	params.domain = domain;
+} else {
+	params.domain = "m.sport-express.ru";
+}
+
+//selectAdSet param
+var SelectAdSet = getParameterByName("SelectAdSet");
+if ((SelectAdSet!==null) && (SelectAdSet!==undefined) && (SelectAdSet!=="")) {
+	params.SelectAdSet = SelectAdSet;
+}
+
+var paramsDiv = window.top.document.createElement('div');
+paramsDiv.id = "probtn_additional_params";
+paramsDiv.innerHTML = JSON.stringify(params);
+paramsDiv.style.cssText = "display: none;";
+window.top.document.body.appendChild(paramsDiv);
+
+try {
+	window.frameElement.style.cssText = "display: none; border: 0px; width: 0px; height: 0px; margin: 0px; padding: 0px;";
+} catch (ex) {
+}
+
+
+loadJS('//cdn.probtn.com/probtn_concat.js', function () {
+});
+
+})();
