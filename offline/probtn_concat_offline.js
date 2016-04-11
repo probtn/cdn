6bec00cd7624345067515f723356ec876c190bef
Anton Lenev <m0rg0t.Anton@gmail.com>
2016-04-11 16:37:25 +0300

    adfox return code

--- offline/probtn_concat_offline.js
+++ offline/probtn_concat_offline.js
@@ -5513,13 +5513,70 @@ function probtn_callPlayer(frame_id, func, args) {
 			                        } else {
 			                            switch (ProBtnControl.currentDomain) {
 			                                case "justlady.ru":
-			                                    $("body").append('<script type="text/javascript">if ("undefined" == typeof pr) var pr = Math.floor(4294967295 * Math.random()) + 1; "undefined" != typeof document.referrer ? "undefined" == typeof afReferrer && (afReferrer = encodeURIComponent(document.referrer)) : afReferrer = ""; var addate = new Date, scrheight = "", scrwidth = ""; if (self.screen) scrwidth = screen.width, scrheight = screen.height; else if (self.java) { var jkit = java.awt.Toolkit.getDefaultToolkit(), scrsize = jkit.getScreenSize(); scrwidth = scrsize.width, scrheight = scrsize.height } document.write(\'<script type="text/javascript" src="//ads.adfox.ru/170600/prepareCode?pp=g&amp;ps=vvq&amp;p2=eszb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=\' + pr + "&amp;pt=b&amp;pd=" + addate.getDate() + "&amp;pw=" + addate.getDay() + "&amp;pv=" + addate.getHours() + "&amp;prr=" + afReferrer + "&amp;pk=imho%20adbutton&amp;puid1=&amp;puid2=&amp;puid3=&amp;puid4=&amp;puid5=&amp;puid6=&amp;puid7=&amp;puid8=&amp;puid9=&amp;puid10=&amp;puid11=&amp;puid12=&amp;puid13=&amp;pdw=" + scrwidth + "&amp;pdh=" + scrheight + \'"></script>\');</script>');
+			                                    if (typeof (pr) == 'undefined') { var pr = Math.floor(Math.random() * 4294967295) + 1; }
+			                                    if (typeof (document.referrer) != 'undefined') {
+			                                        if (typeof (afReferrer) == 'undefined') {
+			                                            afReferrer = encodeURIComponent(document.referrer);
+			                                        }
+			                                    } else {
+			                                        afReferrer = '';
+			                                    }
+			                                    var addate = new Date();
+			                                    var scrheight = '', scrwidth = '';
+			                                    if (self.screen) {
+			                                        scrwidth = screen.width;
+			                                        scrheight = screen.height;
+			                                    } else if (self.java) {
+			                                        var jkit = java.awt.Toolkit.getDefaultToolkit();
+			                                        var scrsize = jkit.getScreenSize();
+			                                        scrwidth = scrsize.width;
+			                                        scrheight = scrsize.height;
+			                                    }
+			                                    document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/170600/prepareCode?pp=g&amp;ps=vvq&amp;p2=eszb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr + '&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pk=imho%20adbutton&amp;puid1=&amp;puid2=&amp;puid3=&amp;puid4=&amp;puid5=&amp;puid6=&amp;puid7=&amp;puid8=&amp;puid9=&amp;puid10=&amp;puid11=&amp;puid12=&amp;puid13=&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
 			                                    break;
 			                                case "kakprosto.ru":
-			                                    $("body").append('<script type="text/javascript">if ("undefined" == typeof pr) var pr = Math.floor(4294967295 * Math.random()) + 1; "undefined" != typeof document.referrer ? "undefined" == typeof afReferrer && (afReferrer = encodeURIComponent(document.referrer)) : afReferrer = ""; var addate = new Date, scrheight = "", scrwidth = ""; if (self.screen) scrwidth = screen.width, scrheight = screen.height; else if (self.java) { var jkit = java.awt.Toolkit.getDefaultToolkit(), scrsize = jkit.getScreenSize(); scrwidth = scrsize.width, scrheight = scrsize.height } document.write(\'<script type="text/javascript" src="//ads.adfox.ru/170600/prepareCode?pp=i&amp;ps=vgo&amp;p2=eszb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=\' + pr + "&amp;pt=b&amp;pd=" + addate.getDate() + "&amp;pw=" + addate.getDay() + "&amp;pv=" + addate.getHours() + "&amp;prr=" + afReferrer + "&amp;pk=imho%20hmpd%20adbutton&amp;puid1=&amp;puid2=&amp;puid3=&amp;puid4=&amp;puid5=&amp;puid6=&amp;puid7=&amp;puid8=&amp;puid9=&amp;puid10=&amp;puid11=&amp;puid12=&amp;puid13=&amp;pdw=" + scrwidth + "&amp;pdh=" + scrheight + \'"></script>\');</script>');
+			                                    if (typeof (pr) == 'undefined') { var pr = Math.floor(Math.random() * 4294967295) + 1; }
+			                                    if (typeof (document.referrer) != 'undefined') {
+			                                        if (typeof (afReferrer) == 'undefined') {
+			                                            afReferrer = encodeURIComponent(document.referrer);
+			                                        }
+			                                    } else {
+			                                        afReferrer = '';
+			                                    }
+			                                    var addate = new Date();
+			                                    var scrheight = '', scrwidth = '';
+			                                    if (self.screen) {
+			                                        scrwidth = screen.width;
+			                                        scrheight = screen.height;
+			                                    } else if (self.java) {
+			                                        var jkit = java.awt.Toolkit.getDefaultToolkit();
+			                                        var scrsize = jkit.getScreenSize();
+			                                        scrwidth = scrsize.width;
+			                                        scrheight = scrsize.height;
+			                                    }
+			                                    document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/170600/prepareCode?pp=i&amp;ps=vgo&amp;p2=eszb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr + '&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pk=imho%20hmpd%20adbutton&amp;puid1=&amp;puid2=&amp;puid3=&amp;puid4=&amp;puid5=&amp;puid6=&amp;puid7=&amp;puid8=&amp;puid9=&amp;puid10=&amp;puid11=&amp;puid12=&amp;puid13=&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
 			                                    break;
 			                                case "pinme.ru":
-			                                    $("body").append('<script type="text/javascript">if("undefined"==typeof pr)var pr=Math.floor(4294967295*Math.random())+1;"undefined"!=typeof document.referrer?"undefined"==typeof afReferrer&&(afReferrer=encodeURIComponent(document.referrer)):afReferrer="";var addate=new Date,scrheight="",scrwidth="";if(self.screen)scrwidth=screen.width,scrheight=screen.height;else if(self.java){var jkit=java.awt.Toolkit.getDefaultToolkit(),scrsize=jkit.getScreenSize();scrwidth=scrsize.width,scrheight=scrsize.height}document.write(\'<script type="text/javascript" src="//ads.adfox.ru/170600/prepareCode?pp=g&amp;ps=birg&amp;p2=eszb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=\'+pr+"&amp;pt=b&amp;pd="+addate.getDate()+"&amp;pw="+addate.getDay()+"&amp;pv="+addate.getHours()+"&amp;prr="+afReferrer+"&amp;pk=imho%20adbutton&amp;puid1=&amp;puid2=&amp;puid3=&amp;puid4=&amp;puid5=&amp;puid6=&amp;puid7=&amp;puid8=&amp;puid9=&amp;puid10=&amp;puid11=&amp;puid12=&amp;puid13=&amp;pdw="+scrwidth+"&amp;pdh="+scrheight+\'"></script>\');</script>');
+			                                    if (typeof (pr) == 'undefined') { var pr = Math.floor(Math.random() * 4294967295) + 1; }
+			                                    if (typeof (document.referrer) != 'undefined') {
+			                                        if (typeof (afReferrer) == 'undefined') {
+			                                            afReferrer = encodeURIComponent(document.referrer);
+			                                        }
+			                                    } else {
+			                                        afReferrer = '';
+			                                    }
+			                                    var addate = new Date();
+			                                    var scrheight = '', scrwidth = '';
+			                                    if (self.screen) {
+			                                        scrwidth = screen.width;
+			                                        scrheight = screen.height;
+			                                    } else if (self.java) {
+			                                        var jkit = java.awt.Toolkit.getDefaultToolkit();
+			                                        var scrsize = jkit.getScreenSize();
+			                                        scrwidth = scrsize.width;
+			                                        scrheight = scrsize.height;
+			                                    }
+			                                    document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/170600/prepareCode?pp=g&amp;ps=birg&amp;p2=eszb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr + '&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pk=imho%20adbutton&amp;puid1=&amp;puid2=&amp;puid3=&amp;puid4=&amp;puid5=&amp;puid6=&amp;puid7=&amp;puid8=&amp;puid9=&amp;puid10=&amp;puid11=&amp;puid12=&amp;puid13=&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
 			                                    break;
 			                                default:
 			                                    break;
