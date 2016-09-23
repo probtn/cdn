2b2c238b245b258fab225e2bba8104077603c723
Anton Lenev <m0rg0t.Anton@gmail.com>
2016-09-23 18:22:47 +0300

    send Version data at getClientSettings

--- offline/probtn_concat_offline.js
+++ offline/probtn_concat_offline.js
@@ -979,7 +979,7 @@ function probtn_callPlayer(frame_id, func, args) {
 			            realDomain: document.domain.replace("www.", ""),
 			            initializedActiveZones: {},
 			            //curent app version
-			            mainVersion: "1.7.1550_17082016_dev",
+			            mainVersion: "1.8.1814_23092016_dev",
 			            hintText: undefined, //hinttext object with additional functions
 			            pizzabtn: undefined,
 			            wrapper: undefined,
@@ -1727,7 +1727,7 @@ function probtn_callPlayer(frame_id, func, args) {
 			                        } else {
 			                            if (ProBtnControl.params.isServerCommunicationEnabled !== false) {
 			                                var receiveMessage = function (event) {
-			                                    //console.log("DeviceCID event", event);
+			                                    console.log("DeviceCID event", event);
 			                                    if ((event.data.type !== undefined) && (event.data.type !== null) && (event.data.type === "probtnCID") && (event.origin === "https://cdn.probtn.com")) {
 			                                        ProBtnControl.cookieFunctions.createCookie("probtnCID", event.data.cid, 365);
 			                                        ProBtnControl.DeviceCID = event.data.cid;
@@ -1897,7 +1897,7 @@ function probtn_callPlayer(frame_id, func, args) {
 			                        campaignId = "&CampaignID=" + ProBtnControl.params.CampaignID;
 			                    }
 
-			                    var url = ProBtnControl.serverUrl + "/1/functions/" + path + "?BundleID=" + ProBtnControl.currentDomain + "&DeviceType=web" + campaignId + "&Version=1.0&DeviceUID=" + probtnId + "&DeviceCUID=" + probtncid + "&localDomain=" + ProBtnControl.realDomain + additional_params + "X-ProBtn-Token=b04bb84b22cdacb0d57fd8f8fd3bfeb8ad430d1b" + "&Location[Longitude]=" + ProBtnControl.geolocation.longitude + "&Location[Latitude]=" + ProBtnControl.geolocation.latitude + "&ScreenResolutionX=" + ProBtnControl.userData.screenHeight + "&ScreenResolutionY=" +
+			                    var url = ProBtnControl.serverUrl + "/1/functions/" + path + "?BundleID=" + ProBtnControl.currentDomain + "&DeviceType=web" + campaignId + "&Version="+ ProBtnControl.mainVersion +"&DeviceUID=" + probtnId + "&DeviceCUID=" + probtncid + "&localDomain=" + ProBtnControl.realDomain + additional_params + "X-ProBtn-Token=b04bb84b22cdacb0d57fd8f8fd3bfeb8ad430d1b" + "&Location[Longitude]=" + ProBtnControl.geolocation.longitude + "&Location[Latitude]=" + ProBtnControl.geolocation.latitude + "&ScreenResolutionX=" + ProBtnControl.userData.screenHeight + "&ScreenResolutionY=" +
 			                        ProBtnControl.userData.screenWidth + "&retina=" + ProBtnControl.userData.retina + "&ConnectionSpeed=" + ProBtnControl.userData.kbs + "&AdditionalTargetingParam=" + ProBtnControl.params.AdditionalTargetingParam + "&callback=?";
 
 			                    if ((params_object == null) || (params_object == undefined)) {
--- probtn_full.js
+++ probtn_full.js
@@ -938,7 +938,7 @@ probtn_initTrackingLinkTest();
             realDomain: document.domain.replace("www.", ""),
             initializedActiveZones: {},
             //curent app version
-            mainVersion: "1.7.1550_17082016_dev",
+            mainVersion: "1.8.1814_23092016_dev",
             hintText: undefined, //hinttext object with additional functions
             pizzabtn: undefined,
             wrapper: undefined,
@@ -1686,7 +1686,7 @@ probtn_initTrackingLinkTest();
                         } else {
                             if (ProBtnControl.params.isServerCommunicationEnabled !== false) {
                                 var receiveMessage = function (event) {
-                                    //console.log("DeviceCID event", event);
+                                    console.log("DeviceCID event", event);
                                     if ((event.data.type !== undefined) && (event.data.type !== null) && (event.data.type === "probtnCID") && (event.origin === "https://cdn.probtn.com")) {
                                         ProBtnControl.cookieFunctions.createCookie("probtnCID", event.data.cid, 365);
                                         ProBtnControl.DeviceCID = event.data.cid;
@@ -1856,7 +1856,7 @@ probtn_initTrackingLinkTest();
                         campaignId = "&CampaignID=" + ProBtnControl.params.CampaignID;
                     }
 
-                    var url = ProBtnControl.serverUrl + "/1/functions/" + path + "?BundleID=" + ProBtnControl.currentDomain + "&DeviceType=web" + campaignId + "&Version=1.0&DeviceUID=" + probtnId + "&DeviceCUID=" + probtncid + "&localDomain=" + ProBtnControl.realDomain + additional_params + "X-ProBtn-Token=b04bb84b22cdacb0d57fd8f8fd3bfeb8ad430d1b" + "&Location[Longitude]=" + ProBtnControl.geolocation.longitude + "&Location[Latitude]=" + ProBtnControl.geolocation.latitude + "&ScreenResolutionX=" + ProBtnControl.userData.screenHeight + "&ScreenResolutionY=" +
+                    var url = ProBtnControl.serverUrl + "/1/functions/" + path + "?BundleID=" + ProBtnControl.currentDomain + "&DeviceType=web" + campaignId + "&Version="+ ProBtnControl.mainVersion +"&DeviceUID=" + probtnId + "&DeviceCUID=" + probtncid + "&localDomain=" + ProBtnControl.realDomain + additional_params + "X-ProBtn-Token=b04bb84b22cdacb0d57fd8f8fd3bfeb8ad430d1b" + "&Location[Longitude]=" + ProBtnControl.geolocation.longitude + "&Location[Latitude]=" + ProBtnControl.geolocation.latitude + "&ScreenResolutionX=" + ProBtnControl.userData.screenHeight + "&ScreenResolutionY=" +
                         ProBtnControl.userData.screenWidth + "&retina=" + ProBtnControl.userData.retina + "&ConnectionSpeed=" + ProBtnControl.userData.kbs + "&AdditionalTargetingParam=" + ProBtnControl.params.AdditionalTargetingParam + "&callback=?";
 
                     if ((params_object == null) || (params_object == undefined)) {
