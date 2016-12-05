859946f754518032001b0feb0ca29c36831cddda
Anton Lenev <m0rg0t.Anton@gmail.com>
2016-12-05 19:17:24 +0300

    tema iframe button

---
+++ iframe_buttons/tema/tema_file_04@2x_edge.js
@@ -0,0 +1,9 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x4='rgba(0,0,0,0)',zx='scaleX',g='image',zy='scaleY',e8='${pic}',x7='rgba(255,255,255,0.00)',i='none';var g5='pic.png',g6='button2.png';var im='',aud='',vid='',js='',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'pic',t:g,r:['0px','0px','500px','449px','auto','auto'],f:[x4,im+g5,'0px','0px'],tf:[[],[],[],['0.85','0.85']]},{id:'button2',t:g,r:['0px','360px','500px','140px','auto','auto'],f:[x4,im+g6,'0px','0px']}],style:{'${Stage}':{isStage:true,r:['null','null','500px','500px','auto','auto'],overflow:'hidden',f:[x7,[270,[['rgba(255,255,255,0.00)',0],['rgba(255,255,255,1.00)',100]]]]}}},tt:{d:30000,a:y,data:[["eid27",zy,1165,250,"easeInOutBack",e8,'1','0.85'],["eid28",zy,1415,585,"easeInOutBack",e8,'0.85','1'],["eid25",zx,1165,250,"easeInOutBack",e8,'1','0.85'],["eid26",zx,1415,585,"easeInOutBack",e8,'0.85','1']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-199612520");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30000,function(sym,e){sym.play(0)});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-199612520");
