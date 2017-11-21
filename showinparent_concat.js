ca7e8a43db5414898998b0aab32a8344c0ac4cfc
Anton Lenev <m0rg0t.Anton@gmail.com>
2017-11-21 18:35:47 +0300

    showinparent_concat - updated version

--- showinparent_concat.js
+++ showinparent_concat.js
@@ -53,6 +53,11 @@ getParamFromUrl('CreativeId');
 getParamFromUrl('UseGeoLocation');
 getParamFromUrl('WaitForGeoLocation');
 
+if ((domain === "baby.ru") || (domain === "m.baby.ru")) {
+	console.log("test - check without interaction with window.top");
+	return;
+}
+
 var paramsDiv = window.top.document.createElement('div');
 paramsDiv.id = "probtn_additional_params";
 paramsDiv.innerHTML = JSON.stringify(params);
@@ -68,11 +73,6 @@ try {
 
 if ((domain===null) || (domain===undefined) || (domain==="")) {
 	domain = document.domain.replace("www.", "");
-}
-
-if ((domain === "baby.ru") || (domain === "m.baby.ru")) {
-	console.log("test - check without interaction with window.top");
-	return;
 }
 
 if ((domain === "babyblog.ru") || (domain === "m.babyblog.ru")) { //eception for babyblog
