318e69d7083e09f329c769955299c2fee3a1bcdb
Anton Lenev <m0rg0t.Anton@gmail.com>
2018-09-10 18:59:19 +0300

    emails

---
+++ emails/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) { 
+	grunt.initConfig({
+	    inlinecss: {
+	        main: {
+	            options: {
+	            	webResources: {
+	            		'images': false
+	            	}
+	            },
+	            files: {
+	                'email_inlined_auto.html': 'email.html',
+	                'email2_inlined_auto.html': 'email2.html',
+	                'email_africa1_inlined_auto.html': 'email_africa1.html'
+	            },
+
+	        }
+	    }
+	});
+
+	grunt.loadNpmTasks('grunt-inline-css');
+
+  	// Default task(s).
+  	grunt.registerTask('default', ['inlinecss']);
+}
